f5789acaef098647feae4918459535a534524dc8
btb0 <benburke2016@gmail.com>
2025-02-08 21:01:01 -0500

    refactor

--- pdfToJson.mjs
+++ pdfToJson.mjs
@@ -10,23 +10,35 @@ pdfParser.on("pdfParser_dataError", (errData) =>
 );
 // on("pdfParser_dataReady") is called when the parser finishes parsing the PDF
 pdfParser.on("pdfParser_dataReady", (pdfData) => {
-    const rawText = pdfParser.getRawTextContent();
-    console.log(pdfData);
-
-    // Save full JSON for debugging purposes
-    fs.writeFileSync("debug.json", JSON.stringify(pdfData, null, 2));
-    console.log('JSON Saved for Debugging');
-
-    // Extracting text manually
-    const textArray = pdfData.Pages.flatMap(page =>
-        page.Texts.map(textItem => decodeURIComponent(textItem.R[0].T))
-    );
-
-    const textContent = textArray.join(" ");
-    console.log("Extracted Text: ", textContent);
-    fs.writeFileSync("output.txt", textContent, "utf8");
-    console.log("Manually extracted text saved to a txt file");
-
+    
+    console.log("raw data ==>", pdfData);
+    extractJSON(pdfData);
+    extractRawText(pdfData);
+    extractFieldTypes(pdfData);
 });
 
 pdfParser.loadPDF(testPdf);
+
+// Extracting text manually
+function extractRawText(pdfData) {
+    const textArray = pdfData.Pages.flatMap(page =>
+        page.Texts.map(textItem => decodeURIComponent(textItem.R[0].T))
+    );
+    
+    const textContent = textArray.join(" ");
+    fs.writeFileSync("output.txt", textContent, "utf8");
+    console.log("Extracted text saved to a .txt file");
+}
+
+// Save full JSON for debugging purposes
+function extractJSON(pdfData) {
+    fs.writeFileSync("debug.json", JSON.stringify(pdfData, null, 2));
+    console.log("JSON saved for debugging");
+}
+
+function extractFieldTypes(pdfData) {
+    // not working
+    fs.writeFileSync("fields.json", JSON.stringify(pdfParser.getAllFieldsTypes()), () => {
+        console.log("Field types saved to fields.json");
+    });
+}
